765223076328500743fba983c3e307b2af222f7a
Afroza Akter Prova <afroza2prova@gmail.com>
2022-10-13 10:41:43 +0600

    Update Todo_listController.php

--- app/Http/Controllers/Todo_listController.php
+++ app/Http/Controllers/Todo_listController.php
@@ -7,16 +7,15 @@ use Illuminate\Http\Request;
 use App\Http\Requests;
 use App\Http\Controllers\Controller;
 use App\Models\Category;
-use App\Models\ToDo_list;
+use App\Models\Todo_list;
 
 class Todo_listController extends Controller
 {
     public function index(Request $request)
     {
         $category = Category::orderBy('created_at', 'asc')->get();
-        $todo_list = ToDo_list::orderBy('created_at', 'asc')->get();
- 
-    return view('welcome', [
+        $todo_list = Todo_list::orderBy('created_at', 'asc')->get();
+        return view('welcome', [
         'todo_list' => $todo_list , 'category' => $category
     ]);
     }
@@ -26,7 +25,7 @@ class Todo_listController extends Controller
         'name' => 'required|max:255',
     ]);
     
-    $data = new ToDo_list();
+    $data = new Todo_list();
     $data->name = $request->name;
     $data->category_id = $request->category_id;
     $data->save();
@@ -35,7 +34,7 @@ class Todo_listController extends Controller
     }
     public function destroy(Request $request, $id)
     {
-        Category::findOrFail($id)->delete();
+        Todo_list::findOrFail($id)->delete();
         return redirect('/');
     }
 }
